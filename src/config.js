cf00e43908d10688569f96a86d6582dcf630ce49
Larissa <102563632+larissabrum@users.noreply.github.com>
2022-05-24 14:41:09 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ let contentDiv = document.getElementsByClassName('content');
 for(let i=0; i<toggles.length; i++){
     toggles[i].addEventListener('click', ()=>{
        if( parseInt(contentDiv[i].style.height)
-        !== contentDiv[i].scrollHeight){
+        != contentDiv[i].scrollHeight){
             contentDiv[i].style.height = contentDiv
             [i].scrollHeight + "px";
            toggles[i].style.color = "#87974D"; 
