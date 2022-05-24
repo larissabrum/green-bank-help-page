15e4695181dc503ead6d5c0a9047f82d0c4f1a66
Larissa <102563632+larissabrum@users.noreply.github.com>
2022-05-24 14:37:49 -0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,24 @@
+let toggles = document.getElementsByClassName('btn');
+let contentDiv = document.getElementsByClassName('content');
+
+for(let i=0; i<toggles.length; i++){
+    toggles[i].addEventListener('click', ()=>{
+       if( parseInt(contentDiv[i].style.height)
+        !== contentDiv[i].scrollHeight){
+            contentDiv[i].style.height = contentDiv
+            [i].scrollHeight + "px";
+           toggles[i].style.color = "#87974D"; 
+        }
+        else{
+          contentDiv[i].style.height = "0px";
+          toggles[i].style.color = "#111130";
+        }
+
+        for(let j=0; j<contentDiv.length; j++){
+            if(j!==i){
+                contentDiv[j].style.height = "0px";
+                toggles[j].style.color = "#111130"; 
+            }
+        }
+    });
+}
